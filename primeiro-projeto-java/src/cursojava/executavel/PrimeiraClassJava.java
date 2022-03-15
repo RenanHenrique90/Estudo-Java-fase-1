8c193944b3c2a9625d784840ff0bab85f17a5e1e
Renan Henrique <renan.henri90@icloud.com>
2022-03-15 16:41:07 -0300

    Fazendo operações entre numeros inteiros

--- primeiro-projeto-java/src/cursojava/executavel/PrimeiraClassJava.java
+++ primeiro-projeto-java/src/cursojava/executavel/PrimeiraClassJava.java
@@ -2,18 +2,14 @@ package cursojava.executavel;
 
 public class PrimeiraClassJava {
 	
-	/*testando comentarios adsada*/
 		public static void main(String[] args) {
-			System.out.println("Hello World" + "Primeiro codigo em java :)");
-			System.out.println("Java é uma linguagem orientada a objeto");
 			
-			int menorIdade = 17;
+			int nota1 = 90;
+			int nota2 = 70;
+			int nota3 = 50;
+			int nota4 = 85;
 			
-			int maiorIdade = 18;
-			
-			int idosoIdade = 65;
-			
-			System.out.println("Menor de idade é abaixo de: " + menorIdade + "\nMaior de idade é acima de: " + maiorIdade + "\nIdoso é acima da idade: " + idosoIdade);
+			System.out.println("Media das notas = " + (nota1 + nota2 + nota3 + nota4)/4);
 			
 		}
 }
