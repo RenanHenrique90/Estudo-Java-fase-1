f47b2a533cc93c708e6e28066d36945aa9213cfa
Renan Henrique <renan.henri90@icloud.com>
2022-03-15 08:53:24 -0300

    Testando

--- primeiro-projeto-java/src/cursojava/executavel/PrimeiraClassJava.java
+++ primeiro-projeto-java/src/cursojava/executavel/PrimeiraClassJava.java
@@ -1,8 +1,10 @@
 package cursojava.executavel;
 
 public class PrimeiraClassJava {
+	
+	/*testando comentarios */
 		public static void main(String[] args) {
-			System.out.println("Hello World");
+			System.out.println("Hello World" + "Primeiro codigo em java :)");
 			System.out.println("Java é uma linguagem orientada a objeto");
 		}
 }
