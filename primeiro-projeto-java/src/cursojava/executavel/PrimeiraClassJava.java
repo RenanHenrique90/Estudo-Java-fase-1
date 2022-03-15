a216b03cdc9f38f51fe46647e71db91d6ba5b7c9
Renan Henrique <renan.henri90@icloud.com>
2022-03-15 15:27:30 -0300

    Testando tipos de variaveis int mais contatenação

--- primeiro-projeto-java/src/cursojava/executavel/PrimeiraClassJava.java
+++ primeiro-projeto-java/src/cursojava/executavel/PrimeiraClassJava.java
@@ -6,5 +6,14 @@ public class PrimeiraClassJava {
 		public static void main(String[] args) {
 			System.out.println("Hello World" + "Primeiro codigo em java :)");
 			System.out.println("Java é uma linguagem orientada a objeto");
+			
+			int menorIdade = 17;
+			
+			int maiorIdade = 18;
+			
+			int idosoIdade = 65;
+			
+			System.out.println("Menor de idade é abaixo de: " + menorIdade + "\nMaior de idade é acima de: " + maiorIdade + "\nIdoso é acima da idade: " + idosoIdade);
+			
 		}
 }
