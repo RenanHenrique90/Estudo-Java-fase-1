0fe76942f38d1cb36785066f5fad738ac4a4570e
Renan Henrique <renan.henri90@icloud.com>
2022-03-15 17:37:56 -0300

    Operações logicas usando IF e Else

--- primeiro-projeto-java/src/cursojava/executavel/PrimeiraClassJava.java
+++ primeiro-projeto-java/src/cursojava/executavel/PrimeiraClassJava.java
@@ -1,15 +1,19 @@
 package cursojava.executavel;
 
 public class PrimeiraClassJava {
-	
-		public static void main(String[] args) {
-			
-			int nota1 = 90;
-			int nota2 = 70;
-			int nota3 = 50;
-			int nota4 = 85;
-			
-			System.out.println("Media das notas = " + (nota1 + nota2 + nota3 + nota4)/4);
-			
+
+	public static void main(String[] args) {
+
+		int mediaAluno = 100;
+
+		if (mediaAluno >= 70 && mediaAluno <= 100) {
+			System.out.println("Voce foi aprovado!");
+		} else if (mediaAluno < 70 && mediaAluno >= 55) {
+			System.out.println("Voce ficou de recuperação");
+		} else if (mediaAluno < 50) {
+			System.out.println("Voce foi reprovado!");
+		} else {
+			System.out.println("Nota invalida!");
 		}
+	}
 }
