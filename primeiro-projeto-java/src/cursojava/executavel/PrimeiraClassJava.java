35b95b1af46b2f6b088a3391889518201a4d3d1a
Renan Henrique <renan.henri90@icloud.com>
2022-03-15 08:37:46 -0300

    Primeiro Projeto Java

---
+++ primeiro-projeto-java/src/cursojava/executavel/PrimeiraClassJava.java
@@ -0,0 +1,8 @@
+package cursojava.executavel;
+
+public class PrimeiraClassJava {
+		public static void main(String[] args) {
+			System.out.println("Hello World");
+			System.out.println("Java é uma linguagem orientada a objeto");
+		}
+}
