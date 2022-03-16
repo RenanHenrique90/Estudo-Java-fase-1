b6f0a6e7f01c48604f13136e16ec5aa86c795825
Renan Henrique <renan.henri90@icloud.com>
2022-03-16 08:33:08 -0300

    Alteração no modo do calculo da media

--- primeiro-projeto-java/src/cursojava/executavel/PrimeiraClassJava.java
+++ primeiro-projeto-java/src/cursojava/executavel/PrimeiraClassJava.java
@@ -4,8 +4,13 @@ public class PrimeiraClassJava {
 
 	public static void main(String[] args) {
 
-		int mediaAluno = 100;
-
+		int nota1 = 100;
+		int nota2 = 50;
+		int nota3 = 70;
+		int nota4 = 80;
+		
+		int mediaAluno = (nota1 + nota2 + nota3 + nota4) / 4;
+		
 		if (mediaAluno >= 70 && mediaAluno <= 100) {
 			System.out.println("Voce foi aprovado!");
 		} else if (mediaAluno < 70 && mediaAluno >= 55) {
